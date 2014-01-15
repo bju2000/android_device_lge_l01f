4500a6a55bc93266d6a2e2c464beaa42a2b122d9
scoty755 <scoty755@gmail.com>
2014-01-15 19:46:52 +0900

    for L01F and LGL22

--- device.mk
+++ device.mk
@@ -1,30 +1,24 @@
-#
-# Copyright (C) 2011 The Android Open-Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product-if-exists, vendor/lge/d802/d802-vendor.mk)
-$(call inherit-product, device/lge/g2-common/g2.mk)
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-## overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+$(call inherit-product-if-exists, vendor/lge/L01F/g2-vendor.mk)
 
+DEVICE_PACKAGE_OVERLAYS += device/lge/L01F/overlay
 
-PRODUCT_PROPERTY_OVERRIDES += \
-	telephony.lteOnGsmDevice=1 \
-	ro.telephony.default_network=9
+LOCAL_PATH := device/lge/g2
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernAl
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
 
 PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+    $(LOCAL_KERNEL):kernel \
+    $(LOCAL_PATH)/recovery/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
 
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_NAME := lge_L01F
+PRODUCT_BRAND := LGE
