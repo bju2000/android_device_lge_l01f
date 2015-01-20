2c0620d1b2dd34235c2a948ea0d05b562f8a78c6
scoty755 <scoty755@gmail.com>
2015-01-21 00:11:14 +0900

    Truncate the NFC

--- device.mk
+++ device.mk
@@ -30,9 +30,9 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
 
 # NFC packages
-PRODUCT_PACKAGES += \
-    nfc_nci.bcm2079x.default \
-    NfcNci
+# PRODUCT_PACKAGES += \
+#    nfc_nci.bcm2079x.default \
+#    NfcNci
 
 # GPS
 PRODUCT_COPY_FILES += \
